0d0655f93982a3082bab6522472bfe26f4e45fa0
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-15 00:46:21 +0530

    added display balance function

--- script.js
+++ script.js
@@ -79,6 +79,13 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+
 const createUsernames = function (accounts) {
   accounts.forEach(function (acc) {
     acc.username = acc.owner
@@ -89,4 +96,3 @@ const createUsernames = function (accounts) {
   });
 };
 createUsernames(accounts);
-
